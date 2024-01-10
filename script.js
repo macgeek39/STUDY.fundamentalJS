64baaafaf75a9b584e4b2cf656e1303959a42cd5
Taiwei Ko <152499488+macgeek39@users.noreply.github.com>
2024-01-10 05:12:36 -0600

    Learned data types and variables

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
+/*
 let js = 'amazing';
-if (js == 'amazing') alert('Javascript is fun');
+// if (js == 'amazing') alert('Javascript is fun');
+
+// let can change value
+let age = 40;
+age = 3;
+
+// const cannot change value
+const firstName = 'mike';
+firstName = 'john';
+
+*/
